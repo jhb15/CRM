5a9223ab68f488790a2dd993ddb46bfda9e4d2dc
James Britton <jhbritton96@icloud.com>
2019-06-29 00:16:55 +0100

    Finally added ability to parse CSV files using CsvHelper Nuget Package. Could be simpler but using manual methods instead of automatic allowed me more controll over the data. Will try simplifying in future.

--- CRM/Models/Car.cs
+++ CRM/Models/Car.cs
@@ -1,16 +1,7 @@
-using System;
-
 namespace CRM.Models
 {
-    public class Car: Vehicle
+    public class Car
     {
-        private string InteriorColour { get; set; }
-
-        public Car(int id, string manufacturer, string model, string registrationNumber, DateTime registrationDate,
-            int engineSize, string interiorColour) : base(id, manufacturer, model, registrationNumber, registrationDate, 
-            engineSize)
-        {
-            InteriorColour = interiorColour;
-        }
+        
     }
 }
--- CRM/Models/Motorcycle.cs
+++ CRM/Models/Motorcycle.cs
@@ -1,17 +1,7 @@
-using System;
-
 namespace CRM.Models
 {
-    public class Motorcycle: Vehicle
+    public class Motocycles : Vehicle
     {
-        public bool HasHelmetStorage { get; set; }
-
-        public Motorcycle(int id, string manufacturer, string model, string registrationNumber,
-            DateTime registrationDate,
-            int engineSize, bool hasHelmetStorage) : base(id, manufacturer, model, registrationNumber, registrationDate,
-            engineSize)
-        {
-            
-        }
+        public string HasHelmetStorage { get; set; } 
     }
 }
